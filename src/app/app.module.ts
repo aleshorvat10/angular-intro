35210fc2b42d3a1e6dc4ba4e19b57fffeb9425f8
ales <ales@dopika.si>
2022-06-16 09:38:55 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TodoComponent
+  ],
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  title:string='';
+  tasks: Task[]=[
+    {title: 'Learn Typescript', completed: false},
+    {title: 'Learn HTML', completed: false},
+    {title: 'Learn english', completed: true}
+  ];
+  constructor() { }
+  deteleTask(taskToDelete: Task)
+  {
+    this.tasks=this.tasks.filter(taks => taks!=taskToDelete);
+    this.Save();
+  }
+  ngOnInit(): void {
+    const taskAsString = localStorage.getItem("tasks");
+    if(taskAsString)
+      this.tasks=JSON.parse(taskAsString);
+    console.table(this.tasks);
+  }
+  add(){
+    this.tasks.push({
+      title: this.title,
+      completed: false
+    })
+    this.title='';
+    this.Save();
+  }
+  Save()
+  {
+    localStorage.setItem("task",JSON.stringify(this.tasks));
+  }
+}
+interface Task{
+  title: string;
+  completed: boolean;
+}
