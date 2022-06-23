0c44602e0eea7910c509b0dd223651470c61c850
ales <ales@dopika.si>
2022-06-23 14:15:07 +0200

    pridobivanje statusa prijave

--- src/app/prijava/prijava.component.ts
+++ src/app/prijava/prijava.component.ts
@@ -7,11 +7,13 @@ import {map, startWith} from 'rxjs/operators';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { MatOptionSelectionChange } from '@angular/material/core';
 
+export interface Response{Status:string;}
 @Component({
   selector: 'app-prijava',
   templateUrl: './prijava.component.html',
   styleUrls: ['./prijava.component.css']
 })
+
 export class PrijavaComponent implements OnInit {
   prijave:Prijave[]=[]
   kartice:Kartice[]=[]
@@ -19,9 +21,11 @@ export class PrijavaComponent implements OnInit {
   myControl = new FormControl<string | Kartice>('');
   filteredOptions: Observable<Kartice[]>;
   selectedUser: string;
+  response: string;
 
   constructor(private dataService: DataService, private router: Router) {
     this.selectedUser='';
+    this.response='';
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => (typeof value === 'string' ? value : value?.OCJ03_Ime)),
@@ -32,6 +36,7 @@ export class PrijavaComponent implements OnInit {
   ngOnInit() {
     this.dataService.getKartice().subscribe(karticeList =>{this.kartice=karticeList})
     this.dataService.getPrijava().subscribe(prijavaList => {this.prijave = prijavaList})
+
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       map(value => (typeof value === 'string' ? value : value?.OCJ03_Ime)),
@@ -51,14 +56,24 @@ export class PrijavaComponent implements OnInit {
     }
 
   async Registracija(){ 
+    var l_Response = "";
     this.p.OCJ01_Id=this.selectedUser.toString();
     this.p.OCJ01_Prihod='2022-06-22 08:01:50';
     console.log(this.selectedUser);
-    this.dataService.savePrijava(this.p).subscribe(_ => {
-      //Go back to the home page
-      this.router.navigate(['/'])
+    this.dataService.savePrijava(this.p).subscribe((val) => 
+    {
+      console.log(val);     
+      let r: Response = <Response>val;
+      if(r.Status=="1")
+        this.response="Prijava";
+      else if(r.Status=="2")
+        this.response="Odjava";
+      else
+        this.response="Napaka";
     });
     await new Promise(f => setTimeout(f, 1000));
+    console.log(l_Response);
+
     this.Reload();
   }
   Reload()
